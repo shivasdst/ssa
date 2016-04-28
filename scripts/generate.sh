4d6773b4a6afb73a01eb244e091068f30016f473
shivasdst <shivably04sdst@gmail.com>
2016-04-28 10:46:56 +0530

    updated generate file

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,6 +5,6 @@ db="ssa"
 usr="root"
 pwd="mysql"
 
-echo "DROP DATABASE IF EXISTS ssa; CREATE DATABASE ssa;" | /usr/bin/mysql -uroot -pmysql
+echo "DROP DATABASE IF EXISTS ssa; CREATE DATABASE ssa;" | /usr/bin/mysql -u$usr -p$pwd
 
 perl insert_toc.pl $host $db $usr $pwd
